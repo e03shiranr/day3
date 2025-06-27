6f02769d77cb77949acff900cdd977390316e806
e03shiranr <e03shiranr@gmail.com>
2025-06-27 15:37:30 +0530

    20250627_5

--- datentime.py
+++ datentime.py
@@ -35,3 +35,20 @@ print(f"formatted date: {formatted_date}")
 
 formatted_time = now.strftime("%I:%M %p")
 print(f"Formatted time: {formatted_time}")
+
+future_date = now + datetime.timedelta(days=7)
+print(f"7 days from now: {future_date}")
+
+past_date = now - datetime.timedelta(hours=7)
+print(f"7 hors ago: {past_date}")
+
+date1 = datetime.datetime(2024, 1, 1)
+date2 = datetime.datetime(2024, 3, 16)
+difference = date2 - date1
+print(f"Dfference between dates: {difference}")
+print(f"Difference in days: {difference.days}")
+print(f"Difference in seconds: {difference.total_seconds()}")
+
+sri_lanka_offset = datetime.timezone(datetime.timedelta(hours=5, minutes=30))
+now_sri_lanka = datetime.datetime.now(sri_lanka_offset)
+print(f"current time of sri lanka is: {now_sri_lanka}")
